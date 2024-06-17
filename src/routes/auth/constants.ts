be861c95edae302886a88a8d42fde39f2f4ed68c
Paul Macfarlane <pauljosemacfarlane@gmail.com>
2024-06-17 12:33:57 -0400

    setup auth url for google sign in

---
+++ src/routes/auth/constants.ts
@@ -0,0 +1,2 @@
+export const GOOGLE_CSRF_STATE_NAME = "csrf_state";
+export const GOOGLE_COOKIE_PATH = "/auth/google";
---
+++ src/routes/auth/google/callback/+server.ts
@@ -0,0 +1,58 @@
+// src/routes/auth/google/callback.ts
+import { redirect, type RequestHandler } from "@sveltejs/kit";
+import {
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  GOOGLE_REDIRECT_UI,
+} from "$env/static/private";
+import axios from "axios";
+
+export const GET: RequestHandler = async ({ url, cookies }) => {
+  const code = url.searchParams.get("code");
+
+  if (!code) {
+    return new Response("Code not found", { status: 400 });
+  }
+
+  const state = url.searchParams.get("state");
+
+  if (state !== cookies.get("csrf_state")) {
+    return new Response("Invalid CSRF token", { status: 400 });
+  }
+
+  cookies.delete("csrf_state", { httpOnly: true, path: "/auth/google" });
+
+  try {
+    const tokenResponse = await axios.post(
+      "https://oauth2.googleapis.com/token",
+      {
+        code,
+        client_id: GOOGLE_CLIENT_ID,
+        client_secret: GOOGLE_CLIENT_SECRET,
+        redirect_uri: GOOGLE_REDIRECT_UI,
+        grant_type: "authorization_code",
+      },
+    );
+
+    const accessToken = tokenResponse.data.access_token;
+
+    const userInfoResponse = await axios.get(
+      "https://www.googleapis.com/oauth2/v2/userinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+
+    const user = userInfoResponse.data;
+
+    console.log(user);
+
+    // TODO: Handle user data (e.g., save to database, create session, etc.)
+  } catch (error) {
+    return new Response("Authentication failed", { status: 500 });
+  }
+
+  throw redirect(302, "/");
+};
