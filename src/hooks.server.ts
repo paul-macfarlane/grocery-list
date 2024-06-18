2aca00b7bc13ab5af457d3f10d0242f3887c597b
Paul Macfarlane <pauljosemacfarlane@gmail.com>
2024-06-17 21:59:17 -0400

    add missing hooks

---
+++ src/hooks.server.ts
@@ -0,0 +1,22 @@
+import { type Handle, redirect } from "@sveltejs/kit";
+import { getUserSession } from "$lib/repository/userSessions";
+
+export const handle: Handle = async ({ event, resolve }) => {
+  const userSession = await getUserSession(event.cookies);
+
+  const authedRoute =
+    !event.url.pathname.startsWith("/auth") ||
+    event.url.pathname === "/auth/logout";
+
+  if (!authedRoute && userSession) {
+    console.log("redirecting user already authed");
+    throw redirect(302, "/");
+  }
+
+  if (authedRoute && !userSession) {
+    console.log("redirecting user not authed");
+    throw redirect(302, "/auth");
+  }
+
+  return resolve(event);
+};
