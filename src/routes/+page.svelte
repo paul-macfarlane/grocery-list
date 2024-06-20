7bd6dbb551ea7b4a091e683046820bbc6f4ba3f1
Paul Macfarlane <pauljosemacfarlane@gmail.com>
2024-06-19 22:52:47 -0400

    navbar wip - pre-componentization

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+    import userProfileSvg from '$lib/assets/user-profile.svg';
+    import logoutSvg from '$lib/assets/logout.svg';
+
     const {data} = $props()
     let profileMenuOn = $state(false)
 
     // todo: these will eventually link to subpaths, and this html will be in a layout (or at least the navbar will)
+    // todo nav elements will also need to show as selected depending on path
     const navLinks = [
         {
             href: "/",
@@ -21,45 +25,45 @@
     const profileMenuLinks = [
         {
             href: "/auth/logout",
-            name: "Logout"
+            name: "Logout",
+            icon: logoutSvg
         }
     ]
-
-    function toggleProfileMenu() {
-        profileMenuOn = !profileMenuOn
-    }
 </script>
 
+<!-- todo this entire nav should be a component -->
 <nav>
-    <ul id="nav-list">
-        {#each navLinks as {href, name}, _}
-            <li>
-                <a class="nav-link" href={href}>{name}</a>
-            </li>
-        {/each}
-    </ul>
+    <div id="nav-container">
+        <ul id="nav-list">
+            {#each navLinks as {href, name}}
+                <li>
+                    <a class="nav-link" href={href}>{name}</a>
+                </li>
+            {/each}
+        </ul>
+    </div>
 
     <div id="profile-menu">
-        <!--  todo add dropdown menu and make this a button that allows user to logout. Need a fallback image too  -->
-        <button onclick={toggleProfileMenu} id="profile-btn">
-            <img id="profile-pic" alt={`${data.user.firstName} ${data.user.lastName}`} src={data.user.profilePicUrl}/>
+        <button onclick={() =>  profileMenuOn = !profileMenuOn} id="profile-btn">
+            <img id="profile-pic" alt={`${data.user.firstName} ${data.user.lastName}`}
+                 src={data.user.profilePicUrl ? data.user.profilePicUrl : userProfileSvg}
+            />
         </button>
 
         {#if profileMenuOn}
-            <ul id="profile-btn-group">
-                {#each profileMenuLinks as {href, name}, _}
-                    <li>
-                        <a class="profile-link" href={href}>{name}</a>
-                    </li>
+            <ul class="profile-menu-group">
+                {#each profileMenuLinks as {href, name, icon}}
+                    <a class="profile-menu-link" href={href}>
+                        <img class="profile-menu-icon" alt="logout" src={icon}>
+                        <span>{name}</span>
+                    </a>
                 {/each}
             </ul>
         {/if}
     </div>
-
 </nav>
 
 <h1>Welcome {data.user.firstName}!</h1>
-
 
 <style>
     :global(body) {
@@ -70,35 +74,40 @@
         display: flex;
         justify-content: space-between;
         align-items: center;
-        border-bottom: 1px solid;
-        border-bottom-color: black;
-        padding: .5em;
+        border-bottom: 1px solid black;
+        padding: 8px;
+    }
+
+    #nav-container {
+        display: flex;
+        flex-grow: 1;
+        justify-content: center;
     }
 
     #nav-list {
         display: flex;
-        justify-content: left;
-        gap: .5em;
+        list-style: none;
+        gap: 8px;
         padding: 0;
     }
-
-    li {
-        list-style-type: none;
-    }
-
 
     .nav-link {
         color: inherit;
         text-decoration: inherit;
-        background-color: greenyellow;
-        padding: .5em;
-        border-radius: .5em;
-        border: 1px solid greenyellow;
+        padding: 8px;
+        border: 1px solid black;
+        border-radius: 8px;
     }
 
     .nav-link:hover, .nav-link:focus {
         border: 1px solid black;
+        background: gold;
         outline: none;
+    }
+
+    #profile-menu {
+        position: relative;
+        margin-left: auto;
     }
 
     #profile-btn {
@@ -110,34 +119,46 @@
     }
 
     #profile-pic {
-        border: 1px solid white;
-        border-radius: 1em;
-        height: 2em;
-        width: 2em;
+        border: 2px solid white;
+        border-radius: 32px;
+        height: 32px;
+        width: 32px;
     }
 
     #profile-pic:hover, #profile-btn:focus-within #profile-pic {
-        border: 1px solid black;
+        border: 2px solid black;
     }
 
-    #profile-btn-group {
-        z-index: 10;
+    .profile-menu-group {
         position: absolute;
-        margin-left: -2.5em;
-        margin-top: -.25em;
-        background-color: white;
+        margin-left: -48px;
+        margin-top: -4px;
+        background: white;
         border: 1px solid black;
-        padding: .5em;
-        width: 3em;
-        border-radius: .25em;
+        padding: 0;
+        display: flex;
+        flex-direction: column;
+        border-radius: 8px;
     }
 
-    #profile-menu {
-        position: relative;
-    }
-
-    .profile-link {
+    .profile-menu-link {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        gap: 8px;
+        padding: 8px;
+        border-radius: 8px;
         color: inherit;
         text-decoration: inherit;
+    }
+
+    .profile-menu-link:hover, .profile-menu-link:focus {
+        background: gold;
+        outline: none;
+    }
+
+    .profile-menu-icon {
+        width: 16px;
+        height: 16px;
     }
 </style>
